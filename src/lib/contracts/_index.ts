3a3fe1c8422a2765d6a7803079cf80e9a3d14825
Declan Rocco <declanrocco@protonmail.com>
2024-06-06 20:00:28 +1000

    Add contract files

---
+++ src/lib/contracts/_index.ts
@@ -0,0 +1,6 @@
+﻿import { environment } from '@environment';
+
+import * as TestnetGenOnePrimeAppContract from "./testnet/gen1/prime/app/contract.json";
+import * as MainnetGenOnePrimeAppContract from "./mainnet/gen1/prime/app/contract.json";
+const GenOnePrimeAppContract = (environment.version == 'testnet') ? TestnetGenOnePrimeAppContract : MainnetGenOnePrimeAppContract;
+export { GenOnePrimeAppContract };
