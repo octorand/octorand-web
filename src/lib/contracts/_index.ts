e536c47f2e2c120dfa8f25c51a1a7a6a3b5b061e
Declan Rocco <declanrocco@protonmail.com>
2024-06-06 20:08:34 +1000

    Export contract files

--- src/lib/contracts/_index.ts
+++ src/lib/contracts/_index.ts
@@ -1,1 +1,23 @@
 ﻿export { default as GenOnePrimeAppContract } from "./gen1/prime/app/contract.json";
+export { default as GenOnePrimeBuyContract } from "./gen1/prime/buy/contract.json";
+export { default as GenOnePrimeListContract } from "./gen1/prime/list/contract.json";
+export { default as GenOnePrimeMintContract } from "./gen1/prime/mint/contract.json";
+export { default as GenOnePrimeOptinContract } from "./gen1/prime/optin/contract.json";
+export { default as GenOnePrimeOptoutContract } from "./gen1/prime/optout/contract.json";
+export { default as GenOnePrimeRenameContract } from "./gen1/prime/rename/contract.json";
+export { default as GenOnePrimeRepaintContract } from "./gen1/prime/repaint/contract.json";
+export { default as GenOnePrimeUnlistContract } from "./gen1/prime/unlist/contract.json";
+export { default as GenOnePrimeUpgradeContract } from "./gen1/prime/upgrade/contract.json";
+export { default as GenOnePrimeWithdrawContract } from "./gen1/prime/withdraw/contract.json";
+
+export { default as GenTwoPrimeAppContract } from "./gen2/prime/app/contract.json";
+export { default as GenTwoPrimeBuyContract } from "./gen2/prime/buy/contract.json";
+export { default as GenTwoPrimeListContract } from "./gen2/prime/list/contract.json";
+export { default as GenTwoPrimeMintContract } from "./gen2/prime/mint/contract.json";
+export { default as GenTwoPrimeOptinContract } from "./gen2/prime/optin/contract.json";
+export { default as GenTwoPrimeOptoutContract } from "./gen2/prime/optout/contract.json";
+export { default as GenTwoPrimeRenameContract } from "./gen2/prime/rename/contract.json";
+export { default as GenTwoPrimeRepaintContract } from "./gen2/prime/repaint/contract.json";
+export { default as GenTwoPrimeUnlistContract } from "./gen2/prime/unlist/contract.json";
+export { default as GenTwoPrimeUpgradeContract } from "./gen2/prime/upgrade/contract.json";
+export { default as GenTwoPrimeWithdrawContract } from "./gen2/prime/withdraw/contract.json";
