9f4961275e3e9bba2d6904d48a37b977d2e591d2
Declan Rocco <declanrocco@protonmail.com>
2024-06-07 10:56:49 +1000

    Add sidebar

--- src/lib/helpers/sidebar.helper.ts
+++ src/lib/helpers/sidebar.helper.ts
@@ -11,9 +11,43 @@ export class SidebarHelper {
                 {
                     id: 'browse',
                     name: 'Browse',
-                    icon: 'fas fa-piggy-bank',
-                    color: '#8e44ad',
+                    icon: 'fas fa-th-large',
+                    color: '#f1c40f',
                     route: 'browse',
+                },
+                {
+                    id: 'market',
+                    name: 'Market',
+                    icon: 'fas fa-shopping-bag',
+                    color: '#2980b9',
+                    route: 'market',
+                },
+                {
+                    id: 'account',
+                    name: 'Account',
+                    icon: 'fas fa-user',
+                    color: '#e74c3c',
+                    route: 'account',
+                },
+            ]
+        },
+        {
+            id: 'platform',
+            name: 'Platform',
+            items: [
+                {
+                    id: 'tokenomics',
+                    name: 'Tokenomics',
+                    icon: 'fas fa-coins',
+                    color: '#9b59b6',
+                    route: 'tokenomics',
+                },
+                {
+                    id: 'statistics',
+                    name: 'Statistics',
+                    icon: 'fas fa-signal',
+                    color: '#2ecc71',
+                    route: 'statistics',
                 },
             ]
         },
