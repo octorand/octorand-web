00f5df07e3c950e00411a9e041c3c2ddbcfcd0d8
Declan Rocco <declanrocco@protonmail.com>
2024-06-14 20:34:14 +1000

    Add arms skin

--- src/lib/tags/_index.ts
+++ src/lib/tags/_index.ts
@@ -1,5 +1,6 @@
 ﻿export * from './pager/pager.tag';
 export * from './gen1/boxes/boxes.tag';
 export * from './gen1/info/info.tag';
+export * from './gen1/skins/arms/arms.tag';
 export * from './gen2/boxes/boxes.tag';
 export * from './gen2/info/info.tag';
