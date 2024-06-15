7883b6d7c439883511bc67ca43e23eb1d52441db
Declan Rocco <declanrocco@protonmail.com>
2024-06-15 13:07:13 +1000

    Update badge colors

--- src/lib/tags/gen1/badges/badges.tag.ts
+++ src/lib/tags/gen1/badges/badges.tag.ts
@@ -62,7 +62,7 @@ export class GenOneBadgesTag implements OnInit, OnChanges {
 
     this.badgesOne = [];
     for (let i = 0; i < 10; i++) {
-      let background = this.prime.badges.includes(badges[i].name) ? '#1890ff' : '#000000';
+      let background = this.prime.badges.includes(badges[i].name) ? '#1890ff' : '#25282c';
       let color = this.prime.badges.includes(badges[i].name) ? '#ffffff' : '#333333';
 
       this.badgesOne.push({
@@ -76,7 +76,7 @@ export class GenOneBadgesTag implements OnInit, OnChanges {
 
     this.badgesTwo = [];
     for (let i = 10; i < 20; i++) {
-      let background = this.prime.badges.includes(badges[i].name) ? '#1890ff' : '#000000';
+      let background = this.prime.badges.includes(badges[i].name) ? '#1890ff' : '#25282c';
       let color = this.prime.badges.includes(badges[i].name) ? '#ffffff' : '#333333';
 
       this.badgesTwo.push({
