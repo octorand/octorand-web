9cf6cb2dd4d76935a22f7e2d7da6a40996bd5725
Declan Rocco <declanrocco@protonmail.com>
2024-07-02 11:10:23 +1000

    Update mainnet config

--- src/environments/environment.mainnet.ts
+++ src/environments/environment.mainnet.ts
@@ -4,7 +4,7 @@ export const environment = {
   algo_indexer: 'https://mainnet-idx.algonode.cloud',
   algo_explorer: 'https://explorer.perawallet.app',
   analytics_code: '',
-  admin_address: '',
+  admin_address: 'NXZLEEQ35KIZ3V3MHFXOOQNEINVLYAU7JCAOZZTQRCDREO4O34I27KLIXU',
   indexer_page_size: 1000,
   display_page_size: 100,
   platform: {
@@ -13,7 +13,7 @@ export const environment = {
     reserve: 'NP5K3WA5H6SBM36DJDZDMPFDVQPRIX6RAXNZ3H6EIC32K6C75YNAMM6Z44'
   },
   gen1: {
-    manager_address: '',
+    manager_address: 'WLBZZ2XAUWBFUNSRL4GMU7FDIBY3WAT5VFXNLRJJX2G5RRT6JQMRKYNHFU',
     seller_market_share: 90,
     admin_market_share: 10,
     rename_price: 10000000,
@@ -24,54 +24,54 @@ export const environment = {
     contracts: {
       prime: {
         buy: {
-          application_id: 0,
-          application_address: ''
+          application_id: 2141432737,
+          application_address: 'UIPEDBA7DYNKCW5HWSEZZBGE2Z3TGFLURQKUENKJ7I63PWPIOBXF6TPP5E',
         },
         claim: {
-          application_id: 0,
-          application_address: ''
+          application_id: 2141432915,
+          application_address: 'W7GPEL43WASRDS3AVJVZGTTJDI4BWWKOBS7TE5IE7CKS2YCAI5FKVM22GQ',
         },
         list: {
-          application_id: 0,
-          application_address: ''
+          application_id: 2141433525,
+          application_address: 'FBTOFN3SA266US6CLYK5YC3EV24Z4JNCL3TUYCNRV64FLN2O4KUAELP4WE',
         },
         mint: {
-          application_id: 0,
-          application_address: ''
+          application_id: 2141433746,
+          application_address: 'CB4RQ6OHKJVD7I4QVCKABFFOKAY7EXPSFITTMCGHLLVOP2J3SMZU2V23J4',
         },
         optin: {
-          application_id: 0,
-          application_address: ''
+          application_id: 2141433859,
+          application_address: 'NVQB3IMZRYLX4GUMFJKTWRXKN6YPHVNVCW6KUXUCFGN4I3XQCFABZ7Y7HU',
         },
         optout: {
-          application_id: 0,
-          application_address: ''
+          application_id: 2141433934,
+          application_address: 'T4MV2U3KVD5BKM4C2M5FOT2CX2EOP3CJK645AIZP7W65RXLF7A2JXKXO6A',
         },
         rename: {
-          application_id: 0,
-          application_address: ''
+          application_id: 2141434049,
+          application_address: 'AVWHPOB5HHPTYNZ34ENLUHPXKU36445ABRK4C2QMQEUWDLTOASPVF2R3YA',
         },
         repaint: {
-          application_id: 0,
-          application_address: ''
+          application_id: 2141434133,
+          application_address: 'G2NS43EWTB6JHD2KKVMK3VQUA2DJ54JJNEZ6RUXCJDCO7MAE37H6JRJAFQ',
         },
         unlist: {
-          application_id: 0,
-          application_address: ''
+          application_id: 2141434200,
+          application_address: 'GH6EJOPVIA77XHP5DFSE54AHWCGZCZEOH533UUBQHPXOESQ76XA5EINV7M',
         },
         upgrade: {
-          application_id: 0,
-          application_address: ''
+          application_id: 2141434605,
+          application_address: 'CNKUGDMTSHX6ILFUPIREETQNJIKADDXAZSRDVZSZMTXKAM4HVNQSUITYCY',
         },
         withdraw: {
-          application_id: 0,
-          application_address: ''
+          application_id: 2141435280,
+          application_address: 'MOKEE7IE5V2XJVFSHYU2CUSXBRTM6XQTWUMQ4T72HP4HTHYA6JYBZQWZL4',
         }
       }
     }
   },
   gen2: {
-    manager_address: '',
+    manager_address: 'RFBPCQV6JFPUSBADUQBRF7575W2ZM4U5GIS37ZBKT5BZPEW22QR7WEZ53U',
     seller_market_share: 90,
     parent_market_share: 5,
     admin_market_share: 5,
@@ -84,48 +84,48 @@ export const environment = {
     contracts: {
       prime: {
         buy: {
-          application_id: 0,
-          application_address: ''
+          application_id: 2141437348,
+          application_address: 'H5FHGDYEKWMF2GEWKVILDGWL75YPQS3UJ5EXEHZ4TRYXZYBC2GMBZFVDOI',
         },
         claim: {
-          application_id: 0,
-          application_address: ''
+          application_id: 2141437425,
+          application_address: '77NJ3JFKIZXGOVYMEKBK4ASS2MTUXM4WQ46AKGSX4MD2MZMTWW67RO2NZU',
         },
         list: {
-          application_id: 0,
-          application_address: ''
+          application_id: 2141437660,
+          application_address: 'LZ4SJEEVW77DHVFHY33T3MHO74TRYYVQFXEUUCZH4YMUH7TDODSHJZKWTU',
         },
         mint: {
-          application_id: 0,
-          application_address: ''
+          application_id: 2141437705,
+          application_address: 'NR342IDRKYKYJE7O3ZS52NNLU6Q5ZLFYPHWZLGJBA2HF7QMRICYKVCCJ4I',
         },
         optin: {
-          application_id: 0,
-          application_address: ''
+          application_id: 2141437760,
+          application_address: '7DST32XDATEMMAC2TD2GUUC2Z6F2HX44A57WI36V3PEX3FXR2R7YHJ75QE',
         },
         optout: {
-          application_id: 0,
-          application_address: ''
+          application_id: 2141438961,
+          application_address: 'PBPKHFF6TDADTSUQ7V53QLE6TZIJAO5AN7O6WLTW4OUUFSXDCPOXSCUHSY',
         },
         rename: {
-          application_id: 0,
-          application_address: ''
+          application_id: 2141439253,
+          application_address: 'T4BOOLJOXI77XXRCLWXJBIMNOV3E5J6PYWNLGDLOSQC6Z7RTW7TIKDC7LY',
         },
         repaint: {
-          application_id: 0,
-          application_address: ''
+          application_id: 2141439316,
+          application_address: '6T7ERQHQALKHXNWULDM6QT7WSQJEEIFK4FKRE5G3PNXWSRVB2DU5F4E6ZE',
         },
         unlist: {
-          application_id: 0,
-          application_address: ''
+          application_id: 2141439497,
+          application_address: '4FMTLKJNBFHC4DD6O2KYJJE3PTQYJGPRX7OIJS5GHJZOZJIKHP7PH7QTDY',
         },
         upgrade: {
-          application_id: 0,
-          application_address: ''
+          application_id: 2141439838,
+          application_address: '5P6KXJZHMCCGXDDQLE5W5DUTE4NOQ3QUHLTCBC3UCGY46AFNUS6S3YH5BI',
         },
         withdraw: {
-          application_id: 0,
-          application_address: ''
+          application_id: 2141439903,
+          application_address: 'GXZW6NIPVRCTVKFABDI2AGSVNTG2TEAD5A2DUAQ65HZABA3LZBSGYM2ZNU',
         }
       }
     }
