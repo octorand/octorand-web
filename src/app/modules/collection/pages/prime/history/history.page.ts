8e8cbe76b5b7fb80f4ce08fa501a3609cdd81177
Declan Rocco <declanrocco@protonmail.com>
2024-07-15 18:36:18 +1000

    Render prime history

--- src/app/modules/collection/pages/prime/history/history.page.ts
+++ src/app/modules/collection/pages/prime/history/history.page.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { GenOnePrimeClaimContract, GenOnePrimeUpgradeContract, GenTwoPrimeClaimContract, GenTwoPrimeUpgradeContract } from '@lib/contracts';
-import { AppHelper, ChainHelper, DataHelper } from '@lib/helpers';
+import { AppHelper, DataHelper, IndexerHelper } from '@lib/helpers';
 import { AppModel, DataModel, PrimeModel } from '@lib/models';
 import { environment } from '@environment';
 
@@ -27,60 +27,21 @@ export class CollectionPrimeHistoryPage implements OnInit, OnChanges {
   @Input() prime: PrimeModel = new PrimeModel();
 
   /**
-   * Whether a wallet is connected
+   * List of events
    */
-  isConnected: boolean = false;
-
-  /**
-   * Whether wallet is opted into prime asset
-   */
-  isOptedIn: boolean = false;
-
-  /**
-   * Whether prime asset is owned by current wallet
-   */
-  isPrimeOwner: boolean = false;
-
-  /**
-   * Whether legacy asset is owned by current wallet
-   */
-  isLegacyOwner: boolean = false;
-
-  /**
-   * Whether prime is optinable
-   */
-  isOptinable: boolean = false;
-
-  /**
-   * Whether prime is claimable
-   */
-  isClaimable: boolean = false;
-
-  /**
-   * Whether prime is upgradable
-   */
-  isUpgradable: boolean = false;
-
-  /**
-   * Tracking actions
-   */
-  actions = {
-    optinPrime: false,
-    claimPrime: false,
-    upgradePrime: false,
-  };
+  events: Array<any> = [];
 
   /**
    * Construct component
    *
    * @param appHelper
-   * @param chainHelper
    * @param dataHelper
+   * @param indexerHelper
    */
   constructor(
     private appHelper: AppHelper,
-    private chainHelper: ChainHelper,
-    private dataHelper: DataHelper
+    private dataHelper: DataHelper,
+    private indexerHelper: IndexerHelper
   ) { }
 
   /**
@@ -102,197 +63,9 @@ export class CollectionPrimeHistoryPage implements OnInit, OnChanges {
    */
   refreshView() {
     if (this.prime) {
-      this.isConnected = this.app.account ? true : false;
-      this.isOptedIn = this.app.assets.find(a => a.id == this.prime.prime_asset_id) ? true : false;
-      this.isPrimeOwner = this.app.assets.find(a => a.id == this.prime.prime_asset_id && a.amount > 0) ? true : false;
-      this.isLegacyOwner = this.app.assets.find(a => a.id == this.prime.legacy_asset_id && a.amount > 0) ? true : false;
-      this.isOptinable = (this.isConnected && !this.isOptedIn) ? true : false;
-      this.isClaimable = (this.isPrimeOwner && this.prime.owner != this.app.account) ? true : false;
-      this.isUpgradable = (this.isLegacyOwner && !this.prime.badges.includes('Explorer')) ? true : false;
+      this.indexerHelper.lookupApplicationLogs(this.prime.application_id).then((value) => {
+        this.events = value.sort((first, second) => second.timestamp - first.timestamp);
+      });
     }
-  }
-
-  /**
-   * Optin to prime asset
-   */
-  optinPrime() {
-    let baseClient = this.chainHelper.getBaseClient();
-    let algodClient = this.chainHelper.getAlgodClient();
-
-    algodClient.getTransactionParams().do().then((params: any) => {
-      let composer = new baseClient.AtomicTransactionComposer();
-
-      composer.addTransaction({
-        txn: baseClient.makeAssetTransferTxnWithSuggestedParamsFromObject({
-          from: this.app.account,
-          to: this.app.account,
-          assetIndex: this.prime.prime_asset_id,
-          amount: 0,
-          suggestedParams: {
-            ...params,
-            fee: 1000,
-            flatFee: true
-          }
-        })
-      });
-
-      let group = composer.buildGroup();
-
-      let transactions = [];
-      for (let i = 0; i < group.length; i++) {
-        transactions.push(group[i].txn);
-      }
-
-      this.actions.optinPrime = true;
-      this.chainHelper.submitTransactions(transactions).then((response) => {
-        this.actions.optinPrime = false;
-        if (response.success) {
-          this.appHelper.loadAccountDetails();
-          this.appHelper.showSuccess('Opted into prime asset successfully');
-        }
-      });
-    });
-  }
-
-  /**
-   * Claim prime
-   */
-  claimPrime() {
-    let baseClient = this.chainHelper.getBaseClient();
-    let algodClient = this.chainHelper.getAlgodClient();
-
-    let claimContract: any = null;
-    let claimContractId = 0;
-
-    if (this.prime.gen == 1) {
-      claimContract = new baseClient.ABIContract(GenOnePrimeClaimContract);
-      claimContractId = environment.gen1.contracts.prime.claim.application_id;
-    } else {
-      claimContract = new baseClient.ABIContract(GenTwoPrimeClaimContract);
-      claimContractId = environment.gen2.contracts.prime.claim.application_id;
-    }
-
-    algodClient.getTransactionParams().do().then((params: any) => {
-      let composer = new baseClient.AtomicTransactionComposer();
-
-      composer.addMethodCall({
-        sender: this.app.account,
-        appID: claimContractId,
-        method: this.chainHelper.getMethod(claimContract, 'claim'),
-        methodArgs: [
-          this.prime.application_id,
-        ],
-        appForeignAssets: [
-          this.prime.prime_asset_id,
-        ],
-        suggestedParams: {
-          ...params,
-          fee: 2000,
-          flatFee: true
-        }
-      });
-
-      let group = composer.buildGroup();
-
-      let transactions = [];
-      for (let i = 0; i < group.length; i++) {
-        transactions.push(group[i].txn);
-      }
-
-      this.actions.claimPrime = true;
-      this.chainHelper.submitTransactions(transactions).then((response) => {
-        this.actions.claimPrime = false;
-        if (response.success) {
-          this.dataHelper.loadPrimeDetails();
-          this.appHelper.showSuccess('Claimed prime successfully');
-        }
-      });
-    });
-  }
-
-  /**
-   * Upgrade prime
-   */
-  upgradePrime() {
-    let baseClient = this.chainHelper.getBaseClient();
-    let algodClient = this.chainHelper.getAlgodClient();
-
-    let upgradeContract: any = null;
-    let upgradeContractId = 0;
-
-    if (this.prime.gen == 1) {
-      upgradeContract = new baseClient.ABIContract(GenOnePrimeUpgradeContract);
-      upgradeContractId = environment.gen1.contracts.prime.upgrade.application_id;
-    } else {
-      upgradeContract = new baseClient.ABIContract(GenTwoPrimeUpgradeContract);
-      upgradeContractId = environment.gen2.contracts.prime.upgrade.application_id;
-    }
-
-    algodClient.getTransactionParams().do().then((params: any) => {
-      let composer = new baseClient.AtomicTransactionComposer();
-
-      if (!this.isOptedIn) {
-        composer.addTransaction({
-          txn: baseClient.makeAssetTransferTxnWithSuggestedParamsFromObject({
-            from: this.app.account,
-            to: this.app.account,
-            assetIndex: this.prime.prime_asset_id,
-            amount: 0,
-            suggestedParams: {
-              ...params,
-              fee: 1000,
-              flatFee: true
-            }
-          })
-        });
-      }
-
-      composer.addMethodCall({
-        sender: this.app.account,
-        appID: upgradeContractId,
-        method: this.chainHelper.getMethod(upgradeContract, 'upgrade'),
-        methodArgs: [
-          this.prime.application_id,
-        ],
-        appForeignAssets: [
-          this.prime.prime_asset_id,
-        ],
-        suggestedParams: {
-          ...params,
-          fee: 3000,
-          flatFee: true
-        }
-      });
-
-      composer.addTransaction({
-        txn: baseClient.makeAssetTransferTxnWithSuggestedParamsFromObject({
-          from: this.app.account,
-          to: this.prime.application_address,
-          assetIndex: this.prime.legacy_asset_id,
-          amount: 1,
-          suggestedParams: {
-            ...params,
-            fee: 1000,
-            flatFee: true
-          }
-        })
-      });
-
-      let group = composer.buildGroup();
-
-      let transactions = [];
-      for (let i = 0; i < group.length; i++) {
-        transactions.push(group[i].txn);
-      }
-
-      this.actions.upgradePrime = true;
-      this.chainHelper.submitTransactions(transactions).then((response) => {
-        this.actions.upgradePrime = false;
-        if (response.success) {
-          this.appHelper.loadAccountDetails();
-          this.appHelper.showSuccess('Upgraded prime successfully');
-        }
-      });
-    });
   }
 }
