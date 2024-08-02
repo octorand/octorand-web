16ead9e3b5ff8f82fa800f1321c00da6c6391774
Declan Rocco <declanrocco@protonmail.com>
2024-08-02 11:30:35 +1000

    Show item image

--- src/lib/tags/item/image/image.tag.ts
+++ src/lib/tags/item/image/image.tag.ts
@@ -1,15 +1,53 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChange, SimpleChanges } from '@angular/core';
 import { ItemModel } from '@lib/models';
+import { environment } from '@environment';
 
 @Component({
   selector: 'lib-tags-item-image',
   templateUrl: './image.tag.html',
   styleUrls: ['./image.tag.scss'],
 })
-export class ItemImageTag {
+export class ItemImageTag implements OnInit, OnChanges {
 
   /**
    * The item parameters
    */
   @Input() item: ItemModel = new ItemModel();
+
+  /**
+   * Image url
+   */
+  url: string = '';
+
+  /**
+   * Image background
+   */
+  background: string = '';
+
+  /**
+   * Initialize component
+   */
+  ngOnInit() {
+    this.calculate();
+  }
+
+  /**
+   * Component parameters changed
+   *
+   * @param changes
+   */
+  ngOnChanges(changes: SimpleChanges) {
+    let prime: SimpleChange = changes['prime'];
+    if (prime && !prime.firstChange) {
+      this.calculate();
+    }
+  }
+
+  /**
+   * Calculate rendering parameters
+   */
+  calculate() {
+    this.url = environment.image_server + '/' + this.item.image + '?optimizer=image&width=200';
+    this.background = 'hsl(' + Math.random() * 360 + ', 100%, 75%)';
+  }
 }
