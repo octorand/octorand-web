ee2b6353fcc154419ccaa30ef78181ba9b36ed2f
Declan Rocco <declanrocco@protonmail.com>
2024-11-29 14:02:25 +1000

    Purche hearts

--- src/app/modules/platform/pages/games/purchase/purchase.page.ts
+++ src/app/modules/platform/pages/games/purchase/purchase.page.ts
@@ -1,9 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AppHelper } from '@lib/helpers';
+import { AppHelper, ChainHelper } from '@lib/helpers';
 import { AppModel, PlayerModel } from '@lib/models';
 import { AuthService } from '@lib/services';
+import { GameDepositContract } from '@lib/contracts';
 import { Subscription } from 'rxjs';
+import { environment } from '@environment';
 
 @Component({
   selector: 'app-platform-games-purchase',
@@ -33,15 +35,36 @@ export class PlatformGamesPurchasePage implements OnInit, OnDestroy {
   ready: boolean = false;
 
   /**
+   * Manage inputs
+   */
+  inputs = {
+    hearts: 10,
+  };
+
+  /**
+   * Tracking actions
+   */
+  actions = {
+    purchaseHearts: false,
+  };
+
+  /**
+   * Platform asset id
+   */
+  assetId: number = 0;
+
+  /**
    * Construct component
    *
    * @param router
    * @param appHelper
+   * @param chainHelper
    * @param authService
    */
   constructor(
     private router: Router,
     private appHelper: AppHelper,
+    private chainHelper: ChainHelper,
     private authService: AuthService
   ) { }
 
@@ -75,6 +98,8 @@ export class PlatformGamesPurchasePage implements OnInit, OnDestroy {
    * Refresh view state
    */
   refreshView() {
+    this.assetId = environment.platform.asset_id;
+
     let account = this.appHelper.getAccount();
     if (account) {
       if (account.token) {
@@ -101,6 +126,74 @@ export class PlatformGamesPurchasePage implements OnInit, OnDestroy {
       this.player.stars = account.stars;
       this.player.ranking = account.ranking;
     }
+  }
+
+  /**
+   * Purchase hearts
+   */
+  purchaseHearts() {
+    if (!this.inputs.hearts) {
+      this.appHelper.showError('Please enter the hearts to purchase');
+      return;
+    }
+
+    if (Number.isNaN(this.inputs.hearts)) {
+      this.appHelper.showError('Please enter the hearts to purchase');
+      return;
+    }
+
+    let baseClient = this.chainHelper.getBaseClient();
+    let algodClient = this.chainHelper.getAlgodClient();
+
+    let depositContract: any = new baseClient.ABIContract(GameDepositContract);
+    let depositContractId = environment.game.contracts.deposit.application_id;
+
+    algodClient.getTransactionParams().do().then((params: any) => {
+      let composer = new baseClient.AtomicTransactionComposer();
+
+      composer.addMethodCall({
+        sender: this.app.address,
+        appID: depositContractId,
+        method: this.chainHelper.getMethod(depositContract, 'deposit'),
+        methodArgs: [
+          this.inputs.hearts * 1000000,
+        ],
+        suggestedParams: {
+          ...params,
+          fee: 1000,
+          flatFee: true
+        }
+      });
+
+      composer.addTransaction({
+        txn: baseClient.makeAssetTransferTxnWithSuggestedParamsFromObject({
+          from: this.app.address,
+          to: environment.platform.reserve,
+          assetIndex: this.assetId,
+          amount: this.inputs.hearts * 1000000,
+          suggestedParams: {
+            ...params,
+            fee: 1000,
+            flatFee: true
+          }
+        })
+      });
+
+      let group = composer.buildGroup();
+
+      let transactions = [];
+      for (let i = 0; i < group.length; i++) {
+        transactions.push(group[i].txn);
+      }
+
+      this.actions.purchaseHearts = true;
+      this.chainHelper.submitTransactions(transactions).then((response) => {
+        this.actions.purchaseHearts = false;
+        if (response.success) {
+          this.appHelper.showSuccess('Purchased hearts successfully');
+        }
+      });
+    });
   }
 
   /**
