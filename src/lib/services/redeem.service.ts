dbcd2adc911498bca427dde4a99dad6c855e070c
Declan Rocco <declanrocco@protonmail.com>
2024-12-01 21:43:12 +1000

    Redeem stars

---
+++ src/lib/services/redeem.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpHelper } from '@lib/helpers';
+
+@Injectable({ providedIn: 'root' })
+export class RedeemService {
+
+    /**
+     * Construct component
+     *
+     * @param httpHelper
+     */
+    constructor(
+        private httpHelper: HttpHelper
+    ) { }
+
+    /**
+     * Process redeem
+     */
+    async process(prime_generation: number, prime_position: number, stars: number, action: string) {
+        const data = {
+            prime_generation: prime_generation,
+            prime_position: prime_position,
+            stars: stars,
+            action: action
+        };
+        return await this.httpHelper.post('redeem/process', data, true);
+    }
+}
