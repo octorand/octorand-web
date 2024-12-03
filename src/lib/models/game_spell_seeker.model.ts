c6fc8d54a44d575e97e520ffb581160b3689d1b0
Declan Rocco <declanrocco@protonmail.com>
2024-12-03 13:09:14 +1000

    Render spell seeker page

--- src/lib/models/game_spell_seeker.model.ts
+++ src/lib/models/game_spell_seeker.model.ts
@@ -6,6 +6,14 @@ export class GameSpellSeekerModel {
     started: boolean = false;
     ended: boolean = false;
     tries: number = 0;
+    answers: Array<any> = [];
+    inputs: Array<any> = [];
+    rewards: number = 0;
+
+    /**
+     * Letters in alphabet
+     */
+    private alphabet: Array<string> = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
 
     /**
      * Update properties
@@ -20,5 +28,32 @@ export class GameSpellSeekerModel {
         this.started = data.started;
         this.ended = data.ended;
         this.tries = data.tries;
+
+        // Calculate answers property
+        let answers = [];
+        for (let i = 0; i < this.reveal.length; i++) {
+            let answer = this.reveal.charAt(i);
+            if (answer == '-') {
+                answers.push({ index: i, text: answer, revealed: false });
+            } else {
+                answers.push({ index: i, text: answer, revealed: true });
+            }
+        }
+        this.answers = answers;
+
+        // Calculate inputs property
+        let inputs = [];
+        for (let i = 0; i < this.alphabet.length; i++) {
+            let letter = this.alphabet[i];
+            if (this.allowed.split('').includes(letter)) {
+                inputs.push({ index: i, letter: letter, allowed: true });
+            } else {
+                inputs.push({ index: i, letter: letter, allowed: false });
+            }
+        }
+        this.inputs = inputs;
+
+        // Calculate rewards
+        this.rewards = 25 - this.tries;
     }
 }
